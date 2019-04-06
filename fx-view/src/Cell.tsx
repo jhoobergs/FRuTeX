cc1ab947ac91640cc2133084025a110674e0acbc
Alexander Bodard <alexander.bodard@student.kuleuven.be>
2019-04-06 15:19:45 +0200

    Add cell, table, color and value

---
+++ fx-view/src/Cell.tsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './Cell.css';
+
+var bgColor = { Default: "#81b71a",
+                    Blue: "#00B1E1",
+                    Cyan: "#37BC9B",
+                    Green: "#8CC152",
+                    Red: "#E9573F",
+                    Yellow: "#F6BB42",
+};
+
+class Cell extends React.Component<{}, { value: string, color: string }>  {
+    constructor(props: any) {
+      super(props);
+      this.state = {
+        value: ' ',
+        color: bgColor.Default
+      };
+  
+      this.handleValueChange = this.handleValueChange.bind(this);
+    }
+
+    handleValueChange(value: any) {
+        this.setState({
+            value: value
+        });
+    }
+
+    handleColorChange(color: any) {
+        this.setState({
+            color: color
+        });
+    }
+  
+    render() {
+      return (
+        <view>
+            <input type="text" value={this.state.value} style={{backgroundColor: this.state.color}} onChange={(event) =>this.handleValueChange(event.target.value)} />
+        </view>
+      );
+    }
+  }
+
+  export default Cell;
