c2573cf64812c32285bf38e8f7aabba7f7c21d73
Arne Bouillon <arne.bouillon@gmail.com>
2019-04-06 14:35:16 +0200

    Add regex

--- fx-backend/src/parser.py
+++ fx-backend/src/parser.py
@@ -1,36 +1,29 @@
 import os
-from fx_exception import FXException
+import re
+from fx_exception import FXException 
+from statement import Statement
+from expression_parser import make_tree
 
-def parse_config_file(path):
-    
-
-def parse_values(path):
+def parse_file(path):
     with open(path, 'r') as file:
         text = file.readlines()
         
+    statements = []
+    matches = re.findall(r"^([^(\|=)]*)=((\s*\|((\s\s*..*(\n|))*))|([^\n\|]*))",
+                         text,
+                         re.MULTILINE)
     
-
-    
-    
-    
-"""-------------------------------------------------------------------------"""     
-"""                                                                         """     
-"""-------------------------------------------------------------------------"""    
-    
-def parse_file(path):
-    basename, ext = os.path.splitext(filename)
-    
-    if ext != '.fx':
-        return
-    
-    if basename == 'values':
-        parse_values(filename)
+    for match in matches:
+        statements.append(Statement(match[0],
+                                    make_tree(match[3] or match[1])))
+        
 
 def parse_project(directory):
-    fx_files = [filename for filename in os.listdir(directory)
+    fx_files = [directory + '/' + filename for filename in os.listdir(directory)
                     if os.path.splitext(filename)[1] == '.fx']
     
     if not 'config.fx' in fx_files:
         raise FXException('Every FRuTeX project needs a config file')
 
-    parse_config_file(directory + '/config.fx')
+    for path in fx_files:
+        parse_file(path)
