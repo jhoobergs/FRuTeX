7870490dda4d0bc2462b0f906210e493da0edae5
Alexander Bodard <alexander.bodard@student.kuleuven.be>
2019-04-06 20:39:04 +0200

    Add MyTable to wrapper

--- fx-view/src/App.tsx
+++ fx-view/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux'
 import './App.css';
+import MyTable from './CellTable'
 
 import store from './store'
 
@@ -10,6 +11,7 @@ class WrappedApp extends React.Component {
   render () {
     return (
       <Provider store={store}>
+        <MyTable></MyTable>
       </Provider>
     )
   }
