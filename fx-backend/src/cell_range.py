b380d102ec654e9e8d1fb60f5625c5ea7e01539f
Arne Bouillon <arne.bouillon@gmail.com>
2019-04-07 02:52:31 +0200

    Stuff

--- fx-backend/src/cell_range.py
+++ fx-backend/src/cell_range.py
@@ -2,15 +2,20 @@ from fx_exception import FXException
 
 class CellRange:
     def __init__(self, rows, cols):
-        if ':' not in rows:
-            rows += ':' + str(int(rows) + 1)
-            
-            
-        if ':' not in cols:
-            cols += ':' + str(int(cols) + 1)
-            
-        self.rows = tuple(map(int, rows.split(':')))
-        self.cols = tuple(map(int, cols.split(':')))
+        if type(rows) == tuple:
+            self.rows = rows
+            self.cols = cols
+        
+        else:
+          if ':' not in rows:
+              rows += ':' + str(int(rows) + 1)
+              
+              
+          if ':' not in cols:
+              cols += ':' + str(int(cols) + 1)
+              
+          self.rows = tuple(map(int, rows.split(':')))
+          self.cols = tuple(map(int, cols.split(':')))
         
         if not (rows[1] > rows[0] and cols[1] > cols[0]):
             raise FXException('Invalid range: must be ascending')
