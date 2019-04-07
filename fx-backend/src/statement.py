21b0aa73a1b134d6f59e9c241f001bc14d107f4c
Arne Bouillon <arne.bouillon@gmail.com>
2019-04-07 08:37:25 +0200

    I think it works

--- fx-backend/src/statement.py
+++ fx-backend/src/statement.py
@@ -1,4 +1,5 @@
 from cell import Cell
+from expression import Expression
 
 class Statement:
     def __init__(self, attrib, cell_ranges, expression):
@@ -13,7 +14,7 @@ class Statement:
                     cell_dict[(r, c)] = Cell(r, c)
 
                 if not update:
-                    cell_dict[(r, c)].apply_expression(self.attrib, self.expression)
+                    cell_dict[(r, c)].apply_expression(self.attrib, Expression(self.expression.text))
                 else:
                     return cell_dict[(r, c)].update_expression(self.attrib, self.expression, config, cell_dict)
         
